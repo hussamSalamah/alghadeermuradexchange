1c6e2792f9bac72712619c0e7d20d3ec16d85e8c
soraabunaser <110615314+soraabunaser@users.noreply.github.com>
2022-08-09 14:44:18 +0300

    Carousel changes

--- alghadeer/main.js
+++ alghadeer/main.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
-	$('.slider').slick({
-    infinite: true,
-		autoplay:true,
-		autoplaySpeed:1500,
-		arrows:true,
-		slidesToShow:2,
-		slidesToScroll:2
-  })
+	const swiper = new Swiper('.swiper', {
+		// Optional parameters
+		loop: true,
+		autoplay: {
+			delay: 1000,
+			disableOnInteraction: false
+		},
+
+		slidesPerView: 2,
+		spaceBetween: 10,
+	
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
+	
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	
+		// And if we need scrollbar
+		scrollbar: {
+			el: '.swiper-scrollbar',
+		},
+	});
+	
 });
